1b4ce581063c2ce477347be333479927793a3a69
Munir405 <deelagbrainy2@gmail.com>
2023-02-16 16:04:45 -0800

    Usage of print function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * description: this is a program that prints exactly programming is like building a multilingual puzzle
+ * using the function puts.
+ *
+ * return: always (0) (success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+
